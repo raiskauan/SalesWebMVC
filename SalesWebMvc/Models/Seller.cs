a20ae21dade7e5069908e308f1b5e844a188b2db
raiskauan <raiscesar5@gmail.com>
2024-11-07 12:22:16 -0300

    Validation

--- SalesWebMvc/Models/Seller.cs
+++ SalesWebMvc/Models/Seller.cs
@@ -5,20 +5,27 @@ namespace SalesWebMvc.Models;
 
 public class Seller
 {
-    public int Id { get; set; }
-    public string Name { get; set; }
+        public int Id { get; set; }
+    [Required(ErrorMessage = "{0} required")]
+    [StringLength(50,MinimumLength = 3, ErrorMessage = "{0} size should be between {2} and {1}")]
+        public string Name { get; set; }
     [DataType(DataType.EmailAddress)]
-    public string Email { get; set; }
+    [EmailAddress(ErrorMessage = "Enter a valid Email ")]
+    [Required(ErrorMessage = "{0} required")]
+        public string Email { get; set; }
     [Display(Name = "Birth Date")]
     [DataType(DataType.Date)]
     [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
-    public DateTime BirthDate { get; set; }
+    [Required(ErrorMessage = "{0} required")]
+        public DateTime BirthDate { get; set; }
     [Display(Name = "Base Salary")]
     [DisplayFormat(DataFormatString = "{0:F2}", ApplyFormatInEditMode = true)]
-    public double BaseSalary { get; set; }
-    public Department Department { get; set; }
-    public int DepartmentId { get; set; }
-    public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
+    [Required(ErrorMessage = "{0} required")]
+    [Range(100.0,50000.0, ErrorMessage = "{0} must be between {1} and {2}")]
+        public double BaseSalary { get; set; }
+        public Department Department { get; set; }
+        public int DepartmentId { get; set; }
+        public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
 
 
     public Seller()
